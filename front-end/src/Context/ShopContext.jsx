0a8ddb08512e47feb4f3d4a8befb9f43bd9129e4
rohit220604 <rjrohit2264@gmail.com>
2024-10-26 23:41:01 +0530

    Frontend almost completed just minor some changes if needed

--- front-end/src/Context/ShopContext.jsx
+++ front-end/src/Context/ShopContext.jsx
@@ -16,12 +16,34 @@ const ShopContextProvider = (props) => {
     
     const addToCart=(itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
-        // console.log(cartItems);
+        // console.log(car tItems);
     }
     const removeFromCart=(itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
     }
-    const contextValue={all_product,cartItems,addToCart,removeFromCart};
+
+    const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems)
+        {
+            let itemInfo=all_product.find((product)=>product.id===Number(item))
+            totalAmount+=itemInfo.new_price*cartItems[item];
+        }
+        return totalAmount;
+    }
+
+    const getTotalCartItems=()=>{
+        let totalItem = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item] > 0)
+            {
+                totalItem+=cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+    const contextValue={getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
     return(
         <ShopContext.Provider value={contextValue}>
             {props.children}
